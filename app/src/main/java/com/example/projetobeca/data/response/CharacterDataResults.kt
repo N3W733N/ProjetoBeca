f79b2214aeff4d8658afb0381e3617150bc0a49a
ProjetoBeca <niltinhosupriano@gmail.com>
2020-09-14 20:13:26 -0300

    Projeto final

--- app/src/main/java/com/example/projetobeca/data/response/CharacterDataResults.kt
+++ app/src/main/java/com/example/projetobeca/data/response/CharacterDataResults.kt
@@ -4,6 +4,7 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
+
 data class CharacterDataResults(
     @Json(name = "data")
     val data: CharacterResultsResponse
